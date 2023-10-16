d999807170b73b683d33fcec553e2b0df8dde1e7
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-10-16 11:13:49 +0200

    fix: ajout de cle primaire dans une table existante

--- src/Creator/BaseCreator.php
+++ src/Creator/BaseCreator.php
@@ -1050,7 +1050,7 @@ class BaseCreator
         $sqls = [];
         $fk   = $this->foreignKeys;
 
-        if (empty($this->fields)) {
+        if ([] === $this->fields) {
             $this->fields = array_flip(array_map(
                 static fn ($columnName) => $columnName->name,
                 $this->db->getFieldData($this->db->prefix . $table)
@@ -1059,20 +1059,18 @@ class BaseCreator
 
         $fields = $this->fields;
 
-        if (! empty($this->keys)) {
+        if ([] !== $this->keys) {
             $sqls = $this->_processIndexes($this->db->prefix . $table, true);
+        }
 
-            $pk = $this->_processPrimaryKeys($table, true);
-
-            if ($pk !== '') {
-                $sqls[] = $pk;
-            }
+        if ([] !== $this->primaryKeys) {
+            $sqls[] = $this->_processPrimaryKeys($table, true);
         }
 
         $this->foreignKeys = $fk;
         $this->fields      = $fields;
 
-        if (! empty($this->foreignKeys)) {
+        if ([] !== $this->foreignKeys) {
             $sqls = array_merge($sqls, $this->_processForeignKeys($table, true));
         }
 
