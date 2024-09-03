1c2ba1e8e1cecff3b5ec47e27ef109ab86c63e5f
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-09-03 20:55:36 +0200

    ammed: modification des evenements en fonction de la nouvelle approche du framework

--- src/Listeners/DatabaseListener.php
+++ src/Listeners/DatabaseListener.php
@@ -9,7 +9,7 @@
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace BlitzPHP\Database\Events;
+namespace BlitzPHP\Database\Listeners;
 
 use BlitzPHP\Contracts\Database\ConnectionResolverInterface;
 use BlitzPHP\Contracts\Event\EventInterface;
